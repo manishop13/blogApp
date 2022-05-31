2252778860462152c9dfdb0d239a5e898a5f2020
manishop13 <manishsharm9196@gmail.com>
2022-06-01 00:36:25 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+//bring in mongoose
+const mongoose = require('mongoose');
+
+//bring in method override
+const methodOverride = require('method-override');
+
+const blogRouter = require('./routes/blogs');
+const Blog = require('./models/Blog');
+const app = express();
+const port= process.env.PORT || 5000 
+//connect to mongoose
+mongoose.connect('mongodb://localhost:27017/crudblog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+//set template engine
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+//route for the index
+app.get('/', async (request, response) => {
+  let blogs = await Blog.find().sort({ timeCreated: 'desc' });
+
+  response.render('index', { blogs: blogs });
+});
+
+app.use(express.static('public'));
+app.use('/blogs', blogRouter);
+
+//listen port
+app.listen(`${port}`,function(req,res){
+  console.log("server is running on port 5000");
+});
